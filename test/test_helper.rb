60d775a5ef51f6894f775aaa2a5fe307decc959e
ouyun <ouxyun@gmail.com>
2018-01-27 20:07:45 +0900

    fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,8 @@ class ActiveSupport::TestCase
     post login_path, params:{ :session => {:account=> user.nil? ? User.first.account : user.account}}
   end
 
-  def set_owner_user_id(tbl)
-    tbl.update_all(:owner_user_id => User.first.id)
+  def set_owner_user_id(obj)
+    user_id = {:owner_user_id => User.first.id}
+    !!(defined? obj.update_all) ? obj.update_all(user_id) : obj.update(user_id)
   end
 end
